671869a5fe27255032ac40119ceea449d1da98ad
IvanShtyrov <144477307+IvanShtyrov@users.noreply.github.com>
2023-09-12 17:51:44 +0300

    Update HomeWork1.py

--- HomeWork1.py
+++ HomeWork1.py
@@ -1,7 +1,78 @@
 #Task1
-# Запрашиваем у пользователя число n
+# Запрашиваем на число n
 n = int(input("Введите число n: "))
 # Используем формулу для вычисления суммы первых n положительных чисел
 sum_of_numbers = (n*(n+1))/2
 # Выводим результат на экран
 print (f"Сумма первых {n} положительных чисел равна {sum_of_numbers}")
+
+#Task2
+import math
+# Запрос для ввода двух целых чисел
+num1 = int(input("Введите первое целое число: "))
+num2 = int(input("Введите второе целое число: "))
+
+# Операции
+addition_result = num1 + num2
+print(f"Сложение: {addition_result}")
+substration_result = num1 - num2
+print(f"Вычитание: {substration_result}")
+multiplication_result = num1 * num2
+print(f"Умножение: {multiplication_result}")
+division_result = num1 / num2
+print(f"Деление: {division_result}")
+floor_division_result = num1 // num2
+print(f"Целочисленное деление: {floor_division_result}")
+modulus_result = num1 % num2
+print(f"Остаток от деления: {modulus_result}")
+exponentiation_result = num1 ** num2
+print(f"Возведение в степень: {exponentiation_result}")
+log10_result = math.log10(num1)
+print(f"Логарифм по основанию 10 от первого числа: {round(log10_result,2)}")
+# Операции сравнения
+less_than_result = num1 < num2
+print(f"Меньше чем: {less_than_result}")
+less_equal_result = num1 <= num2
+print(f"Меньше или равно: {less_equal_result}")
+greater_than_result = num1 > num2
+print(f"Больше чем: {greater_than_result}")
+greater_equal_result = num1 >= num2
+print(f"Больше или равно: {greater_equal_result}")
+not_equal_result = num1 != num2
+print(f"Не равно: {not_equal_result}")
+equal_result = num1 == num2
+print(f"Равно: {equal_result}")
+
+
+#Task3
+
+
+#Task4
+
+
+#Task5
+
+
+#Task6
+
+
+#Task7
+
+
+#Task8
+
+
+#Task9
+
+
+#Task10
+
+
+#Task11
+
+
+#Task12
+
+
+#Task13
+
