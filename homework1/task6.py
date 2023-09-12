4ffaa239e7041cd2642a76baf0b8a22388fe1840
IvanShtyrov <144477307+IvanShtyrov@users.noreply.github.com>
2023-09-12 21:14:09 +0300

    Update task6.py

--- homework1/task6.py
+++ homework1/task6.py
@@ -1,1 +1,17 @@
+# Запрос двузначного числа
+two_digit_number = int(input("Введите двузначное число: "))
 
+# Разделяем число на десятки и единицы
+tens_digit = two_digit_number // 10
+ones_digit = two_digit_number % 10
+
+# Вычисляем сумму и произведение цифр
+sum_of_digits = tens_digit + ones_digit
+product_of_digits = tens_digit * ones_digit
+
+# Выводим результат для двузначного числа
+print(f"Сумма цифр: {sum_of_digits}")
+print(f"Произведение цифр: {product_of_digits}")
+
+# Запрос трехзначного числа
+three_digit_number = int(input("Введите трехзначное число: "))
